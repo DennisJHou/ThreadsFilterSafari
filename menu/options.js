01ecd641fc93464cda1c5d12e525b007ea8c9975
DennisJHou <>
2026-06-05 10:38:25 +0800

    Add missing frontend assets: icons, libs, menu, static

---
+++ menu/options.js
@@ -0,0 +1,28 @@
+$("#show_scores").change(function() {
+    if(this.checked)
+        chrome.storage.sync.set({show_scores: true});
+    else
+        chrome.storage.sync.set({show_scores: false});
+});
+
+
+chrome.storage.sync.get(['show_scores'], function (items) {
+    let show_scores = items.show_scores;
+    let active = false;
+    if (show_scores) {
+        $("#show_scores").prop('checked', true);
+        active = true;
+    }
+
+    // const event = new CustomEvent("EnableDebug", {detail: { active: active}});
+    // window.dispatchEvent(event);
+});
+
+let clicks_count = 0;
+
+$("body").click(function (){
+    clicks_count++;
+    if (clicks_count>5){
+        $(".options_container").show();
+    }
+});
